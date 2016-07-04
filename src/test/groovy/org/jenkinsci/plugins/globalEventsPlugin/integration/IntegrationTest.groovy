66bef375889c55a6a606366ed135cd6b63a7ccd1
Nick Grealy <nickgrealy@gmail.com>
2016-07-05 09:26:59 +1000

    #21 Issue successfully reproduced in a test - now to fix it.

--- src/test/groovy/org/jenkinsci/plugins/globalEventsPlugin/integration/IntegrationTest.groovy
+++ src/test/groovy/org/jenkinsci/plugins/globalEventsPlugin/integration/IntegrationTest.groovy
@@ -11,27 +11,67 @@ import org.junit.Test
  */
 class IntegrationTest {
 
+    /**
+     * N.B. Requires a running Jenkins instance on port '19091'.
+     */
     @Test
-    public void testFoobar(){
-        def client = new RESTClient('http://localhost:19091/' )
+    public void issues21() {
 
-        def resp = client.post(
-                path: '/descriptorByName/org.jenkinsci.plugins.globalEventsPlugin.GlobalEventsPlugin/testGroovyCode',
-                body: [ onEventGroovyCode: 'log.info("FOOBAR")' ],
-                requestContentType: ContentType.URLENC )
+        String jenkinsUrl = 'http://localhost:19091/'
 
-        assert resp.status == 200
-        NodeChild text = resp.data
-        assert text.text().replaceAll("\\d+", "X") == '''Execution completed successfully!
+        // Causes -> An exception was caught.java.lang.NoClassDefFoundError: org/apache/ivy/core/report/ResolveReport
+        String groovyCodeInput = '''
+@Grab('org.codehaus.groovy.modules.http-builder:http-builder:0.7.1')
+def test = 123
+log.info("FOOBAR")
+'''
+
+        String expectedOutput = '''Execution completed successfully!
 >>> Executing groovy script - parameters: [env, run, jenkins, log, event, context]
 FOOBAR
 >>> Ignoring response - value is null or not a Map. response=null
->>> Executing groovy script completed successfully. totalDurationMillis='X',executionDurationMillis='X',synchronizationMillis=`X`'''.replaceAll("\\r?\\n", "")
+>>> Executing groovy script completed successfully. totalDurationMillis='X',executionDurationMillis='X',synchronizationMillis=`X`'''
+
+        invokeTestGroovyCode(jenkinsUrl, groovyCodeInput, expectedOutput)
     }
 
-    public static void waitUntil(Closure isTrue, int seconds){
-        for (int i = 0; i < seconds; i++){
+    /**
+     * Invokes the "testGroovyCode" REST service.
+     */
+    public static void invokeTestGroovyCode(String jenkinsUrl, String groovyCodeInput, String expectedOutput) {
+        def client = new RESTClient(jenkinsUrl)
 
+        // don't throw exceptions for >=400 status codes...
+        client.handler.failure = client.handler.success
+
+        // wait up to 1 minute for the jenkins server to start...
+        waitUntil("Server is UP", 60, { client.get(path: '/').status == 200 })
+
+        // call testGroovyCode service...
+        def resp = client.post(
+                path: '/descriptorByName/org.jenkinsci.plugins.globalEventsPlugin.GlobalEventsPlugin/testGroovyCode',
+                body: [onEventGroovyCode: groovyCodeInput],
+                requestContentType: ContentType.URLENC)
+
+        // verify the response was success (with expected output)...
+        assert resp.status == 200
+        NodeChild text = resp.data
+        assert text.text().replaceAll("\\d+", "X") == expectedOutput.replaceAll("\\r?\\n", "")
+    }
+
+    /**
+     * Waits until the given condition is true.
+     */
+    public static void waitUntil(String conditionName, int secondsTimeout, Closure conditionIsTrue) {
+        int i;
+        for (i = 0; i < secondsTimeout && !conditionIsTrue(); i++) {
+            println "Condition '$conditionName' not (yet) met, sleeping for 1 second..."
+            Thread.sleep(1000)
+        }
+        if (i == secondsTimeout) {
+            throw new RuntimeException("Timeout occurred after $secondsTimeout seconds, waiting for condition '$conditionName'.")
+        } else {
+            println "Condition '$conditionName' was met."
         }
     }
 
