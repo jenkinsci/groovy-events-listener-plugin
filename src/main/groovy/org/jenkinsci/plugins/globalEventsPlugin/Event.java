fe744391469b2b77abe873f7aac544fc0b156ef3
vrenjith <v.renjith@gmail.com>
2016-10-14 01:30:28 +0530

    Added Listener for Configuration Changes (#28)
    * Item listener added
    * Added events to the getAll list

--- src/main/groovy/org/jenkinsci/plugins/globalEventsPlugin/Event.java
+++ src/main/groovy/org/jenkinsci/plugins/globalEventsPlugin/Event.java
@@ -27,6 +27,13 @@ public final class Event {
     public static final String QUEUE_BUILDABLE = "QueueListener.onEnterBuildable";
     public static final String QUEUE_LEFT = "QueueListener.onLeft";
 
+    public static final String ITEM_UPDATED = "ItemListener.onUpdated";
+    public static final String ITEM_LOCATION_CHANGED = "ItemListener.onLocationChanged";
+    public static final String ITEM_RENAMED = "ItemListener.onRenamed";
+    public static final String ITEM_DELETED = "ItemListener.onDeleted";
+    public static final String ITEM_COPIED = "ItemListener.onCopied";
+    public static final String ITEM_CREATED = "ItemListener.onCreated";
+
     public static String[] getAll() {
         return new String[] {
             PLUGIN_STARTED,
@@ -45,6 +52,12 @@ public final class Event {
             QUEUE_BLOCKED,
             QUEUE_BUILDABLE,
             QUEUE_LEFT,
+            ITEM_CREATED,
+            ITEM_COPIED,
+            ITEM_DELETED,
+            ITEM_RENAMED,
+            ITEM_LOCATION_CHANGED,
+            ITEM_UPDATED,
         };
     }
 }
