fc735ddc22e6e76c451b00f110f1acacf75875da
René Scheibe <rene.scheibe@gmail.com>
2019-04-27 17:17:25 +0200

    Upgrade Cucumber dependency
    There was a warning that the notation '~@tag' is deprecated and
    should be replaced with 'not @tag'. The code was adapted accordingly.
    
    This version improves HTML report file generation. There are no more
    report files per scenerio, only per package and test class. This fixes
    the problem that report generation failed under Windows. This was because
    scenarios with their parameters contain e.g. pipes and these are no valid
    characters for filenames under Windows.

--- src/test/groovy/org/jenkinsci/plugins/globalEventsPlugin/acceptance/WipAcceptanceTests.java
+++ src/test/groovy/org/jenkinsci/plugins/globalEventsPlugin/acceptance/WipAcceptanceTests.java
@@ -8,7 +8,7 @@ import org.junit.runner.RunWith;
 @CucumberOptions(
         strict = false,
         plugin = {"pretty"},
-        tags = {"@wip", "~@ignore"}
+        tags = {"@wip", "not @ignore"}
 )
 public class WipAcceptanceTests {
 }
